c4aacb25c94a2330b5de8e68a67738632607ac6a
github username <github email address>
2020-02-22 14:22:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const BASEURL = "http://localhost:3000/dogs" //BASEURL
+    
+    //FETCHDOGS
+    fetch(BASEURL)
+    .then(resp => resp.json())
+    .then(dogs => {
+    console.log(dogs)
+    })//FETCHDOGS
 
-})
+
+
+
+
+})//DOMContentLoaded
